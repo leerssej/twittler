91cd103fbde5b7640ae8de444c1e0149f2cbd35f
leerssej <jens.leerssen@gmail.com>
2018-05-22 12:00:18 -0700

    update button and timestamp functioning

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   const $tweetlog = $('#tweet-log');
   // $body.html('');
+  let lastIndexLoaded = 11; // accounting for the first ten always getting loaded
 
   console.log(streams.home.length);
   let index = streams.home.length - 1;
@@ -9,21 +10,30 @@ $(document).ready(function(){
     let tweet = streams.home[index];
     let $tweet = $('<div></div>');
     $tweet.text(`@ ${tweet.user}: ${tweet.message} - ${tweet.created_at}`);
-    $tweet.appendTo($tweetlog);
+    $tweet.prependTo($tweetlog);
     index -= 1;
   }
 
-// on button click
-  console.log(streams.home.length);
+const updateTweets = function() {
+  let indexToLoad = lastIndexLoaded;
+  lastIndexLoaded = streams.home.length; // this is preemptive for when loop is finished
+  while(indexToLoad < lastIndexLoaded) {
+console.log('lastIndexLoaded:', lastIndexLoaded);
+console.log('lastIndexLoaded:', lastIndexLoaded);
+    let tweet = streams.home[indexToLoad];
+    let $tweet = $('<div></div>');
+    $tweet.text(`@ ${tweet.user}: ${tweet.message} - ${tweet.created_at}`);
+    $tweet.prependTo($tweetlog);
+    indexToLoad += 1;
+  }
+}
 
-  // let index = streams.home.length - 1;
-  // console.log(index);
-  // while(index >= 0){
-  //   let tweet = streams.home[index];
-  //   let $tweet = $('<div></div>');
-  //   $tweet.text(`@ ${tweet.user}: ${tweet.message}`);
-  //   $tweet.appendTo($body);
-  //   index -= 1;
-  // }
+// on button click
+  $('.buttonContainer button').click(function() {
+    //alert('clicked');
+    console.log(streams.home.length);
+    updateTweets();
+  });
+
 
 });
