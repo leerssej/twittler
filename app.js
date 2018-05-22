3d32189bbe279990ab651ac23edcf9ab0b08b9ca
leerssej <jens.leerssen@gmail.com>
2018-05-22 14:31:30 -0700

    single updateTweets function loading initial tweets and reloading on click styling updated

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
 $(document).ready(function(){
   const $tweetlog = $('#tweet-log');
-  let lastIndexLoaded = 11; // accounting for the first ten always getting loaded
-
-  console.log(streams.home.length);
-  let index = streams.home.length - 1;
-  while(index >= 0){
-    let tweet = streams.home[index];
-    let $tweet = $(
-    '<div class="tweet">' +
-      '<div class="username"></div>' +
-      '<span class="msg"></span>' +
-      '<span class="idx"></span>' +
-      '<div class="timestamp"></div>' +
-    '</div>');
-    $tweet.children('.username').text(`@${tweet.user}:`);
-    $tweet.children('.msg').text(`${tweet.message} `);
-    $tweet.children('.idx').text(`(${index + 1}) `);
-    $tweet.children('.timestamp').text(`- (${tweet.created_at})`);
-    $tweet.prependTo($tweetlog);
-    index -= 1;
-  }
-
+  let lastIndexLoaded = 0; 
+  
   const updateTweets = function() {
     let indexToLoad = lastIndexLoaded;
     lastIndexLoaded = streams.home.length; // this is preemptive for when loop is finished
@@ -35,16 +16,17 @@ $(document).ready(function(){
       '</div>');
       $tweet.children('.username').text(`@${tweet.user}:`);
       $tweet.children('.msg').text(`${tweet.message} `);
-      $tweet.children('.idx').text(`(${index + 1}) `);
+      $tweet.children('.idx').text(`(${indexToLoad + 1}) `);
       $tweet.children('.timestamp').text(`- (${tweet.created_at})`);
-        // let $tweet = $('<div></div>');
-      // $tweet.text(`@ ${tweet.user}: ${tweet.message} - ${tweet.created_at}`);
       $tweet.prependTo($tweetlog);
       indexToLoad += 1;
     }
   }
 
-  // on button click
+  // load tweets on refresh/startup
+  updateTweets()
+
+  // load tweets on displayNew Button click
   $('#displayNewBtn').click(function() {
     updateTweets();
   });
