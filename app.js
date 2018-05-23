1eeac1318f9d38045adcfb1d2571a8032b3f16c7
leerssej <jens.leerssen@gmail.com>
2018-05-23 16:36:53 -0700

    presentation ready

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const writeTweets = tweetStream => {
   const $tweetlog = $('#tweet-log');
   $tweetlog.html('');
   tweetStream.forEach((tweet, i) => {
+    const elapsedTime = timestamp => ((Date.now() - new Date(timestamp)) / 1000).toFixed(2);
     $(`<div class="tweet">
         <div class="username">
             <a class = "userlink" href='javascript:writeTweets(streams.users.${tweet.user})'>@${tweet.user}:</a>
@@ -16,13 +17,14 @@ const writeTweets = tweetStream => {
           </div>
         </div>
         <span class="msg">${tweet.message} </span>
-        <span class="idx">(${i + 1}) </span>
-        <div class="timestamp">${ simplifyTimeStamp(tweet.created_at) }</div>
+        <div class="elapsedTime">${elapsedTime(tweet.created_at)} seconds ago </div>
       </div>
     `).prependTo($tweetlog);
     // console.log(tweet.created_at);
   });
 }
+        // <span class="idx">(${i + 1}) </span>
+        // <div class="timestamp">${ simplifyTimeStamp(tweet.created_at) }</div>
 
 // Hide User Input form field on page load
 
