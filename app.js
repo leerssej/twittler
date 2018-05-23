2355f2e3de6d504e75e96dcc58c78ef673602090
leerssej <jens.leerssen@gmail.com>
2018-05-22 18:39:14 -0700

    Bare Minimum Requirements Satisfied refactored the tweetstream loading scripts back down user timeline load built as an active js hyperlinks

--- app.js
+++ app.js
@@ -1,82 +1,26 @@
-$(document).ready(function(){
+// helper functions
+const simplifyTimeStamp = timestamp => String(timestamp).split(' ').slice(0,5).join(' ');
+
+const writeTweets = tweetStream => {
   const $tweetlog = $('#tweet-log');
-  let lastIndexLoaded = 0; 
+  $tweetlog.html('');
+  tweetStream.forEach((tweet, i) => {
+    $(`<div class="tweet">
+        <div class="username">
+          <a class = "userlink" href='javascript:writeTweets(streams.users.${tweet.user})'>@${tweet.user}:</a>
+        </div>
+        <span class="msg">${tweet.message} </span>
+        <span class="idx">(${i + 1}) </span>
+        <div class="timestamp">${ simplifyTimeStamp(tweet.created_at) }</div>
+      </div>
+    `).prependTo($tweetlog);
+  });
+}
 
-  const updateTweets = function() {
-    let indexToLoad = lastIndexLoaded;
-    lastIndexLoaded = streams.home.length; // this is preemptive for when loop is finished
-    while(indexToLoad < lastIndexLoaded) {
-      let tweet = streams.home[indexToLoad];
-      let simpleTimeStamp = String(tweet.created_at).split(' ').slice(0,5).join(' ');
-      let $tweet = $(
-      '<button class="tweet">' +
-        `@${tweet.user}:` +
-        // '<div class="username"></div>' +
-        // '<span class="msg"></span>' +
-        // '<span class="idx"></span>' +
-        // '<div class="timestamp"></div>' +
-      '</button>');
-      // $tweet.children('.username').text(`@${tweet.user}:`);
-      // $tweet.children('.msg').text(`${tweet.message} `);
-      // $tweet.children('.idx').text(`(${indexToLoad + 1}) `);
-      // $tweet.children('.timestamp').text(`${simpleTimeStamp}`);
-      $tweet.prependTo($tweetlog);
-      indexToLoad += 1;
-    }
-  }
-
-  // // load tweets on refresh/startup
-  updateTweets();
+$(document).ready(() => {
+  // load tweets on refresh/startup
+  writeTweets(streams.home);
   
   // load tweets on displayNew Button click
-  $('#displayNewBtn').on('click', function() {
-    updateTweets();
-  });
-
-  // // display Button on click of username
-  $('.tweet').on('click', function() {
-    console.log($(this)/*.children().text().split(':')[0].slice(1)*/);
-    // console.log($(this).children().text().split(':')[0].slice(1));
-  });
-  
-  // // // displayUserNameTweets
-  // const updateTweetsByUser = function(user) {
-  //   let indexToLoad = lastIndexLoaded;
-  //   lastIndexLoaded = streams.home.length; // this is preemptive for when loop is finished
-  //   while(indexToLoad < lastIndexLoaded) {
-  //     let tweet = streams.home[indexToLoad];
-  //     let simpleTimeStamp = String(tweet.created_at).split(' ').slice(0,5).join(' ');
-  //     let $tweet = $(
-  //     '<div class="tweet">' +
-  //       '<div class="username"></div>' +
-  //       '<span class="msg"></span>' +
-  //       '<span class="idx"></span>' +
-  //       '<div class="timestamp"></div>' +
-  //     '</div>');
-  //     $tweet.children('.username').text(`@${tweet.user}`);
-  //     $tweet.children('.msg').text(`${tweet.message} `);
-  //     $tweet.children('.idx').text(`(${indexToLoad + 1}) `);
-  //     $tweet.children('.timestamp').text(`${simpleTimeStamp}`);
-  //     $tweet.prependTo($tweetlog);
-  //     indexToLoad += 1;
-  //   }
-  // }
-  // let words = [];
-  // $('.tweet' ).bind('mouseup', function() {
-  //   word = window.getSelection().toString();
-  //     if(word != ''){
-  //       if( confirm("Add *"+word+"* to array?") ){words.push(word);}
-  //     }
-  //   });
-  
-  //   //just to see what we've got
-  // $('button').click(function(){alert(words);});
-  // });
-
-
+  $('#updateAllBtn').click(() => writeTweets(streams.home));
 });
-
-
-// $(document).ready(function(){
-  
-// });
